7a50e5382a7e965db16bca0f3b8370f1f997ca32
Evance Masuku <vance97muzi@gmail.com>
2025-10-27 20:12:43 +0200

    Prepare project for Render deployment

--- ecom/build.sh
+++ ecom/build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit  
+# Exit immediately if a command fails
+set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Run migrations
 python manage.py migrate
 
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start gunicorn server
+gunicorn ecom.wsgi
