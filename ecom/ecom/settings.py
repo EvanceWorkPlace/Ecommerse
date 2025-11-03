6fec0c37db7fe91942ff34170cd43e078dda727b
Evance Masuku <vance97muzi@gmail.com>
2025-11-04 00:01:00 +0200

    Trying to solve Bad request (400) part (2)

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -18,7 +18,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost 127.0.0.1 ecommerse-2-4k35.onrender.com"
+    "ecommerse-2-4k35.onrender.com localhost 127.0.0.1"
 ).split()
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
